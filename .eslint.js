b5f7748c2171bb599ead8492e500d469b71c6b88
nisol91 <nicolasolzi91@gmail.com>
2020-11-16 19:09:48 +0100

    again settin up firebase

--- .eslint.js
+++
@@ -1,6 +0,0 @@
-// the code - add the 'vue/no-unused-components' rule
-rules: {
-'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-'vue/no-unused-components': 'off'
-}
