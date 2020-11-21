7f3f6e3d6dfd5c863386684dd990dbec4c6c5a60
nisol91 <nicolasolzi91@gmail.com>
2020-11-21 16:28:31 +0100

    ok registration e email verification

--- src/components/auth/Register.vue
+++ src/components/auth/Register.vue
@@ -6,79 +6,70 @@
           <div class="card-header">Register</div>
           <div class="card-body">
             <div v-if="error" class="alert alert-danger">{{ error }}</div>
-            <form action="#" @submit.prevent="submit">
-              <div class="form-group row">
-                <label for="name" class="col-md-4 col-form-label text-md-right"
-                  >Name</label
-                >
+            <div class="form-group row">
+              <label for="name" class="col-md-4 col-form-label text-md-right"
+                >Name</label
+              >
 
-                <div class="col-md-6">
-                  <input
-                    id="name"
-                    type="name"
-                    class="form-control"
-                    name="name"
-                    value
-                    required
-                    autofocus
-                    v-model="form.name"
-                  />
-                </div>
+              <div class="col-md-6">
+                <input
+                  id="name"
+                  type="name"
+                  class="form-control"
+                  name="name"
+                  value
+                  required
+                  autofocus
+                  v-model="form.name"
+                />
               </div>
+            </div>
 
-              <div class="form-group row">
-                <label for="email" class="col-md-4 col-form-label text-md-right"
-                  >Email</label
-                >
+            <div class="form-group row">
+              <label for="email" class="col-md-4 col-form-label text-md-right"
+                >Email</label
+              >
 
-                <div class="col-md-6">
-                  <input
-                    id="email"
-                    type="email"
-                    class="form-control"
-                    name="email"
-                    value
-                    required
-                    autofocus
-                    v-model="form.email"
-                  />
-                </div>
+              <div class="col-md-6">
+                <input
+                  id="email"
+                  type="email"
+                  class="form-control"
+                  name="email"
+                  value
+                  required
+                  autofocus
+                  v-model="form.email"
+                />
               </div>
+            </div>
 
-              <div class="form-group row">
-                <label
-                  for="password"
-                  class="col-md-4 col-form-label text-md-right"
-                  >Password</label
-                >
+            <div class="form-group row">
+              <label
+                for="password"
+                class="col-md-4 col-form-label text-md-right"
+                >Password</label
+              >
 
-                <div class="col-md-6">
-                  <input
-                    id="password"
-                    type="password"
-                    class="form-control"
-                    name="password"
-                    required
-                    v-model="form.password"
-                  />
-                </div>
+              <div class="col-md-6">
+                <input
+                  id="password"
+                  type="password"
+                  class="form-control"
+                  name="password"
+                  required
+                  v-model="form.password"
+                />
               </div>
+            </div>
 
-              <div class="form-group row mb-0">
-                <div class="col-md-8 offset-md-4">
-                  <v-btn color="primary" rounded dark depressed>
-                    Register
-                  </v-btn>
-                  <div class="text-center">
-                    <v-btn rounded color="primary" dark> Rounded Button </v-btn>
-                    <v-btn tile color="success">
-                      <v-icon left> mdi-pencil </v-icon>
-                      Edit
-                    </v-btn>
-                  </div>
-                </div>
+            <div class="form-group row mb-0">
+              <div class="col-md-8 offset-md-4">
+                <v-btn @click="submit" color="primary" rounded dark depressed>
+                  Register
+                </v-btn>
               </div>
-            </form>
+            </div>
           </div>
         </div>
       </div>
@@ -89,6 +80,7 @@
 
 <script>
 import firebase from "firebase";
+import { db } from "../../main";
 
 export default {
   data() {
@@ -107,19 +99,18 @@ export default {
         .auth()
         .createUserWithEmailAndPassword(this.form.email, this.form.password)
         .then((data) => {
-          data.user
-            .updateProfile({
-              displayName: this.form.name,
-            })
-            .then(() => {
-              // now we have access to the signed in user
-              const user = firebase.auth().currentUser;
-              // send the signed in user a verification email
-              const actionCodeSettings = {
-                url: `${window.location.hostname}/sign-in/?email=${user.email}`,
-              };
-              user.sendEmailVerification(actionCodeSettings);
-            });
+          db.collection("users")
+            .doc(data.user.uid)
+            .set({ name: this.form.name });
+        })
+        .then(() => {
+          // now we have access to the signed in user
+          const user = firebase.auth().currentUser;
+          // send the signed in user a verification email
+          const actionCodeSettings = {
+            url: `https://${window.location.hostname}/auth/login`,
+          };
+          user.sendEmailVerification(actionCodeSettings);
         })
         .catch((err) => {
           this.error = err.message;
