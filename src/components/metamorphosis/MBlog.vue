eddd0274bfbf3415c581c83f3e9c9e0e8d176c73
nisol91 <nicolasolzi91@gmail.com>
2020-12-08 19:33:30 +0100

    wip

--- src/components/metamorphosis/MBlog.vue
+++ src/components/metamorphosis/MBlog.vue
@@ -1,3 +1,19 @@
 <template>
-  <div>blog</div>
+  <div class="mBlogBox">blog</div>
 </template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  created() {
+    this.$store.commit("isMetamorphosis", true);
+  },
+};
+</script>
+<style lang="scss">
+.mBlogBox {
+  width: 100%;
+  min-height: 100vh;
+}
+</style>
